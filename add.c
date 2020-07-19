cfb64b1c4f603b342dff19a430717d5fc789c595
Sarvesh420 <sp627495@gmail.com>
2020-07-19 22:22:55 +0530

    dev1 chaned

--- add.c
+++ add.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c;
+    int a,b,sum;
     printf("Enter two number\n");
     scanf("%d%d",&a,&b);
-    c=a+b;
+    sum=a+b;
     printf("Sum of number is %d",sum);
     return 0;
 }
